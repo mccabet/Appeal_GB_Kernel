c4f785aa3ab69361ac5efc5a99d29de8b5e90525
mccabet <mccabet60446@gmail.com>
2013-02-22 14:31:11 -0600

    Initial commit

---
+++ Kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Thu Feb 21 15:48:42 CST 2013"
+#define LINUX_COMPILE_TIME "15:48:42"
+#define LINUX_COMPILE_BY "tom"
+#define LINUX_COMPILE_HOST "ThinkPad-T410s"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
